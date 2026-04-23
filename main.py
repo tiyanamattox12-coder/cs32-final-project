18cd1d7c0fd736a5579296d2f2278a5f4793e4c5
tiyanamattox12-coder <tiyanamattox12@gmail.com>
2026-04-23 18:14:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ open_madlib = open(current_madlib)
 questions = ['What is the name of your first pet? ', 'What is something that smells weird? ', 'Name your favorite food. ']
 
 # Madlib txt file indication being _n, _v, _a or something of that nature. 
-noun = ['What is the name of your first pet? ']
-verb = []
-adjective = [] 
+noun = ['What is the name of your first pet? ', 'What was your favorite childhood toy?', 'Name a place you've always wanted to vist.', What's an object you use everyday?', 'Name a funny looking animal,', What's a type of food you love?', What is your dream job?', Name your favorite Harry Potter character.', 'What's a holiday you enjoy?', 'Name something you would see in the sky.' ]
+verb = ['Favorite way to get to classes.', 'Something you do when you're bored.', 'Favorite mode of travel.','Something you do before a party.','An action you'd do on a summer day.', 'A verb related to studying.','Something that makes you laugh.','A verb related to the kitchen.']
+adjective = ['Describe your spring semester.','How do you feel today?','Describe your favorite food.','A word for the science center.','Describe the state of your dorm room.','A word describing your favorite song.','Describe Harvard Square.','A word for Annenberg.'] 
 
 replace = ''
 updated_madlib = ''
